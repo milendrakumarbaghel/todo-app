491d36e645ac5280b2f0a4c3d3f1bb4b37a785ed
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2024-10-28 22:23:19 +0530

    added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateTodo from './components/CreateTodo';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
-        Hello
+        <CreateTodo />
+        <Todos />
     </div>
   );
 }
---
+++ frontend/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+        <h2>Go to gym</h2>
+        <h3>You need to go to the gym</h3>
+        <button>Mark as completed</button>
+    </div>
+  )
+}
+
+export default Todos
