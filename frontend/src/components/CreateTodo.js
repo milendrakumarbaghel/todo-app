f249b5fc29c1b229a30d3920dff88492d3aebd62
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2024-10-28 22:14:04 +0530

    Added Frontend

---
+++ frontend/src/components/CreateTodo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+    <div>
+        <input type="text" placeholder="title" /> <br />
+        <input type="text" placeholder="description" /> <br />
+
+        <button>Add a Todo</button>
+    </div>
+  )
+}
+
+export default CreateTodo
